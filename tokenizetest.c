8110e0d2ea9b6ca5f85cafcf56e1b25c884f61b5
Andrew Bishara <60140630+andrewbishara@users.noreply.github.com>
2023-11-18 13:57:23 -0500

    Create tokenizetest.c

---
+++ tokenizetest.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TOKENSIZE 8
+
+typedef struct tokens
+{
+    char *tok;
+    struct token *next;
+} tokens;
+
+
+// Returns 1 if passed argument is a valid character in a command 
+int is_valid(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '*' || c == '.'|| c == '>' || c == '<' || c == '|' || c == '/');
+}
+
+// tokenize gets passed the entire command entered
+// Split command into individual tokens, fills them into a token struct 
+tokens* tokenize(char *command){
+
+    int len = strlen(*command);
+    char line[len];
+    line[0] = *command;
+
+    tokens *head;
+    tokens *curTok = head;
+    
+    char *current[TOKENSIZE] = malloc(TOKENSIZE);
+    int count = 0;
+    int nextNew;
+
+    // for every character in command
+    // returns the head of the linked list of tokens
+    for(int i = 0; i < len; i++){
+
+        if(line[i] == '\n' && i != 0)
+            return head;
+
+        if(line[i] == ' ' && i != 0){
+            if(head->tok == NULL){
+                head->tok = current;
+            }
+            else{
+                curTok->tok = current;
+            }
+            
+            nextNew = 1; 
+            current == NULL;
+        } 
+
+        if(is_valid(line[i])){
+
+            if(count >= TOKENSIZE){
+                realloc(current, sizeof(current) * 2);
+            }
+
+            if(nextNew){
+                curTok->next = malloc(sizeof(tokens));
+                curTok = curTok->next;
+
+                nextNew = 0;
+            }
+
+            current[count] = line[i];
+            count++;
+        }
+
+    }
+
+}
+
+
+
+void main(int argc, char **argv){
+
+    char *command;
+
+    printf("mysh> ");
+    gets(*command);
+
+    tokens *current = tokenize(command);
+    
+    while(current->next != NULL){
+        printf("%c\n", current->tok);
+    }
+
+}
